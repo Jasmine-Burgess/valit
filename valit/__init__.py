3f15137cafe47d433d1f06df413a4af2617996bb
Jasmine-Burgess <j.j.burgess@lancaster.ac.uk>
2025-03-31 17:56:01 +0100

    Create __init__.py

---
+++ valit/__init__.py
@@ -0,0 +1,7 @@
+""" Value iteration method for Markov Decision Processes (MDP). Requires modelling a finite number of states and actions and
+functions determining the transition probabilities and immediate rewards."""
+
+from .valit import value_iteration
+
+_version_ = '1.0.0'
+_author_ = 'Jasmine Burgess'
